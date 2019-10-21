417db4cfaac8e78ed61712646a23e748aaa477fa
Jasper Khonesavanh <jasperkhonesavanh@gmail.com>
2019-10-21 14:05:22 -0700

    GJ

---
+++ main.js
@@ -0,0 +1,32 @@
+function setTime() {
+    var date = new Date()
+
+    var hours = date.getHours()
+    var minutes = date.getMinutes()
+    var seconds = date.getSeconds()
+
+    minutes = ( minutes < 10 ? "0" : "" ) + minutes
+    seconds = ( seconds < 10 ? "0" : "" ) + seconds
+
+    var time = `${hours}:${minutes}:${seconds}`
+    $('.clock').html(time)
+}
+
+setInterval(setTime, 1000)
+
+function color() {
+    var date = new Date()
+
+    var hours = date.getHours()
+    var minutes = date.getMinutes()
+    var seconds = date.getSeconds()
+
+    minutes = ( minutes < 10 ? "0" : "" ) + minutes
+    seconds = ( seconds < 10 ? "0" : "" ) + seconds
+
+    var time = `#${hours}${minutes}${seconds}`
+    $('.color').html(time)
+    $('.color').css("background", time)
+}
+
+setInterval(color, 1000)
